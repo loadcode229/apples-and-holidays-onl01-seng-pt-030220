2884f20b94b44f796704abd5d9a26a6b0cc7d359
Adam Priest <sepone229@gmail.com>
2020-03-15 23:43:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |season, 
 end
 
 def all_holidays_with_bbq(holiday_hash)
