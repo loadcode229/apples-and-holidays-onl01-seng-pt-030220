671225589e441d6b1b25ccd72ede6773402df057
Adam Priest <sepone229@gmail.com>
2020-03-15 23:51:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{supplies.capitalize}"
+      puts 
     end
   end
 end
