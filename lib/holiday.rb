6dc37cdf9546bafd6bcf011f32b9d80e2552a8fc
Adam Priest <sepone229@gmail.com>
2020-03-16 00:01:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,6 +38,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do |holiday, supply|
+    holiday.map do |
     if supply.include?("BBQ")
       holiday << supply
     end
