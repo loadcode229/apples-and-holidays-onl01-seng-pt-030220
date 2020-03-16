dc85e51268661364b7b794969c17bd154c0ff1bc
Adam Priest <sepone229@gmail.com>
2020-03-16 00:31:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,30 +1,20 @@
-require 'pry'
-
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
-
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter][:christmas] << "Balloons"
   holiday_hash[:winter][:new_years] << "Balloons"
 end
-
-
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
-
 end
-
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
-
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].values.flatten
 end
-
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
@@ -33,10 +23,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
-
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supply|
       if supply.include?("BBQ")
