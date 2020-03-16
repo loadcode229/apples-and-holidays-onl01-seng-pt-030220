5d9d1df2615c9854f5b6d9e2af371bc97983d069
Adam Priest <sepone229@gmail.com>
2020-03-16 00:02:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
-      if supply.include?("BBQ")
+      holiday if supply.include?("BBQ")
         holiday << supply
       end
     end
